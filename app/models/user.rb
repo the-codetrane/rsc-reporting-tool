34fde77bcf7dd55569e87b368fba5ffd641400db
Michael Cain <michaelmcain@yahoo.com>
2016-11-02 14:30:58 -0400

    Welcome controller and Devise auth created.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :email, :first_name, :last_name
+
   belongs_to :group
   validates :email, uniqueness: true
   validates :first_name, :password, :email, presence: true
