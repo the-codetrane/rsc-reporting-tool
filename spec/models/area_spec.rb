60315b083cbbd03dec8d88196327dd681a5bc02c
Michael Cain <michaelmcain@yahoo.com>
2016-11-01 21:46:29 -0400

    rspec init and shoulda config

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Area, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Area do
+  describe 'Associations' do
+    it should { }
+  end
 end
