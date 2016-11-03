4f67301fea57d946b3d57b79e291c7bbbe26fb83
Michael Cain <michaelmcain@yahoo.com>
2016-11-02 21:40:31 -0400

    sweet baby jesus Devise works!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
-    end
-    devise_parameter_sanitizer.permit(:account_update) do |user_params|
-      user_params.permit(:first_name, :last_name, :email, :password)
-    end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :password])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :email, :first_name, :last_name
-
   belongs_to :group
 
 end
