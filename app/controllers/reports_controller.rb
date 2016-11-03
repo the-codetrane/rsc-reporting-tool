fe8a0b2750788768361912ffa511688337e64044
Michael Cain <michaelmcain@yahoo.com>
2016-11-03 16:58:03 -0400

    filled out reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,39 @@
 class ReportsController < ApplicationController
+  def index
+    @reports = Report.all.select(:title, :committee)
+  end
+
+  def show
+    @report = Report.find(params[:id])
+  end
+
+  def new
+    @report = Report.new(report_params)
+  end
+
+  def create
+    @report = Report.new
+
+    if @report.save
+      redirect_to :index
+    else
+      flash[:alert] = 'record did not save'
+      redirect_to :new
+    end
+  end
+
+  def edit
+    @report = Report.find(params[:id])
+  end
+
+  def destroy
+    Report.find(params[id]).destroy
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit!
+  end
+
 end
--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe ReportsController, type: :controller do
+describe ReportsController do
 
 end
