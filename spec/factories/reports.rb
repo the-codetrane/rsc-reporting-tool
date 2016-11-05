97b66c59fdc220e75d97e1a7f720a112a26e0763
Michael Cain <michaelmcain@yahoo.com>
2016-11-05 11:47:24 -0400

    found out why assets weren't compiling

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -1,10 +1,11 @@
 FactoryGirl.define do
-  factory :report do
-    title "MyString"
-    committee "MyString"
-    attendees "MyString"
-    old_business "MyText"
-    new_business "MyText"
-    notes "MyText"
+  factory :report do |f|
+    f.title Faker::Company.buzzword << ' Report'
+    f.committee Faker::Company.name << ' Committee'
+    f.attendees "MyString"
+    f.old_business "MyText"
+    f.new_business "MyText"
+    f.created_by
+    f.notes "MyText"
   end
 end
