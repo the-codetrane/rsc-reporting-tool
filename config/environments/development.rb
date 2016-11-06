04bd3760db89fc3baf9f40bf1c0b8e0214101673
Michael Cain <michaelmcain@yahoo.com>
2016-11-05 22:00:10 -0400

    bagged app settings from railscast

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,24 +8,14 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  config.action_mailer.default_url_options = { host: 'naworks.org' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: smtp.gmail.com,
-      port: 587,
-      domain: 'naworks.org',
-      authentication: plain,
-      enable_starttls_auto: true,
-      user_name: ENV['GMAIL_USER'],
-      password: ENV['GMAIL_PASS']
-  }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
