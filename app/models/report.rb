868e3ebd227c3cc6ef7230b8a5060edd5295a9cb
Michael Cain <michaelmcain@yahoo.com>
2016-11-05 21:37:01 -0400

    added new mailer features

--- app/models/report.rb
+++ app/models/report.rb
@@ -6,4 +6,9 @@ class Report < ApplicationRecord
     Committee.find(self.committee_id).name
   end
 
+  def mailer_submitted_by
+    user = User.find_by_email(self.created_by)
+    "Submitted by #{user.na_name} from #{user.group.name}"
+  end
+
 end
