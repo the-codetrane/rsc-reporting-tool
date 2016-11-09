5a56502d19d6e49ef902d53b3b4ac700e8757f29
Michael Cain <michaelmcain@yahoo.com>
2016-11-09 17:31:46 -0500

    working on welcome mailer

--- spec/mailers/previews/welcome_preview.rb
+++ spec/mailers/previews/welcome_preview.rb
@@ -3,7 +3,8 @@ class WelcomePreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/welcome/welcome
   def welcome
-    WelcomeMailer.welcome
+    @user = User.first
+    WelcomeMailer.welcome(@user)
   end
 
 end
