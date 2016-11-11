2727eeca198a8668f3d9d008629733d4d39235b5
Michael Cain <michaelmcain@yahoo.com>
2016-11-11 07:40:50 -0500

    all tests passing

--- spec/mailers/welcome_spec.rb
+++ spec/mailers/welcome_spec.rb
@@ -1,17 +1,18 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe WelcomeMailer, type: :mailer do
-  describe "welcome" do
-    let(:mail) { WelcomeMailer.welcome }
+  describe 'welcome' do
+    let(:user) {FactoryGirl.create(:user)}
+    let(:mail) { WelcomeMailer.welcome(user) }
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Welcome")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["admin@naworks.com"])
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Welcome to the RSC Committee Reporting Tool')
+      expect(mail.to).to eq(['michael.m.cain.ii@gmail.com'])
+      expect(mail.from).to eq(['no-reply@naworks.com'])
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    it 'renders the body' do
+      expect(mail.body.encoded).to match('Hi')
     end
   end
 
