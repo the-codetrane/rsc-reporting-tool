0d51ab256b8fd99f9f19f24ccf149f8b5c3b4b5f
Michael Cain <michaelmcain@yahoo.com>
2016-11-18 16:40:36 -0500

    added tooltips like a BOSS!

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,10 @@
 module WelcomeHelper
+
+  def new_report_tooltip
+    %q(Click here to sumbit a new report)
+  end
+
+  def find_report_tooltip
+    %q(Click here to find a <br /> sumbitted report<br /> <strong>Remember</strong>: you can only edit a report that you submitted)
+  end
 end
