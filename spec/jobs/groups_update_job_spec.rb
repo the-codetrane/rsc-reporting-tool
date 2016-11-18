ec754c11f9825713b3bb02622f35700cb7b6017d
Michael Cain <michaelmcain@yahoo.com>
2016-11-18 14:22:04 -0500

    setting up Sidekiq

--- spec/jobs/groups_update_job_spec.rb
+++ spec/jobs/groups_update_job_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 describe GroupsUpdateJob do
-
+  it 'queues jobs accordingly' do
+    expect(GroupsUpdateJob.perform_later).to change(GroupsUpdateJob.jobs, :size).by(1)
+  end
 end
