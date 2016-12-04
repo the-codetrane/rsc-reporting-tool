a89180c31cb677d2a1555f9e81eb1f85b15dbeb8
Michael Cain <michaelmcain@yahoo.com>
2016-12-04 12:25:44 -0500

    added roles to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105191424) do
+ActiveRecord::Schema.define(version: 20161204172208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 20161105191424) do
     t.index ["committee_id"], name: "index_reports_on_committee_id", using: :btree
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -66,6 +72,7 @@ ActiveRecord::Schema.define(version: 20161105191424) do
     t.string   "last_name"
     t.integer  "group_id"
     t.integer  "committee_id"
+    t.string   "role"
     t.index ["committee_id"], name: "index_users_on_committee_id", using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["group_id"], name: "index_users_on_group_id", using: :btree
