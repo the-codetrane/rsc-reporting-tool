61ba305f8a1dd19c634151e85d8eb370538f54c0
Michael Cain <michaelmcain@yahoo.com>
2016-12-24 11:30:09 -0500

    removing groups from admin dashboard.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,7 +3,6 @@ class AdminController < ApplicationController
 
   def index
     @users = User.includes(:group, :committee, :role).all.order(:last_name).page(params[:member_page]).per(10)
-    @groups = Group.includes(:area).all.order(:name).page(params[:group_page]).per(10)
   end
 
 end
