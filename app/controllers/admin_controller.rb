292c046729aad9671ed88faca438602539717c4c
Michael Cain <michaelmcain@yahoo.com>
2016-12-26 09:42:35 -0500

    paginating admin dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   before_action :is_admin?
 
   def index
-    @users = User.includes(:group, :committee, :role).all.order(:last_name)
+    @users = User.includes(:group, :committee, :role).all.order(:last_name).page(params[:page]).per(10)
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,8 @@ FactoryGirl.define do
     sequence(:last_name) { Faker::Name.last_name }
     sequence(:email) { Faker::Internet.email }
     password Faker::Internet.password
+    sequence(:group_id) { Random.rand(1..380) }
+    sequence(:committee_id) { Random.rand(1..20) }
     role_id 5
 
     factory :admin_user do
