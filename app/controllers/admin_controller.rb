609a0112ad904ca7025f29e6a33d057a35452540
Michael Cain <michaelmcain@yahoo.com>
2016-12-26 09:53:04 -0500

    optimizing views

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,9 @@ class AdminController < ApplicationController
 
   def index
     @users = User.includes(:group, :committee, :role).all.order(:last_name).page(params[:page]).per(10)
+    @roles = Role.all
+    @groups = Group.all
+    @committees = Committee.all
   end
 
 end
