b0e197979b22317964f53fdf479b27bf253cc502
Michael Cain <michaelmcain@yahoo.com>
2017-02-05 10:25:22 -0500

    update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 set :branch, 'master'
 
-role [:web, :db, :app], '77.104.162.56', user: 'naworks4', primary: true
+role [:web, :db, :app], '77.104.162.56', user: 'naworks4', primary: true, port: 18765
