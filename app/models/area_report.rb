f3404829476dfa5f4761f87e5fae53be81dc9a67
Michael Cain <michaelmcain@yahoo.com>
2017-02-05 12:22:41 -0500

    updated schema

--- app/models/area_report.rb
+++ app/models/area_report.rb
@@ -1,2 +1,27 @@
 class AreaReport < ApplicationRecord
+  belongs_to :committee
+  has_one :user, primary_key: :created_by, foreign_key: :email
+
+  def report_committee
+    self.committee.name
+  end
+
+  def mailer_submitted_by
+    if self.created_by?
+      user = User.find_by_email(self.created_by)
+      "Submitted by #{user.na_name} from #{user.group.name}"
+    else
+      "Submitter unknown"
+    end
+  end
+
+  def Report.selector
+    Rails.cache.fetch('report_selector', :expires_in => 5.minutes) do
+      Report.all.order(:name)
+    end
+  end
+
+  def reports_submitted_by
+    "#{self.user.na_name} from #{self.user.group.name}"
+  end
 end
