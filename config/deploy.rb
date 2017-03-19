be2d08e8c3d0b0f8ff02cdb09294fc57d621121c
Michael Cain <michaelmcain@yahoo.com>
2017-03-19 09:13:44 -0400

    updated ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.8.0'
 
 set :application, 'rsc-reporting-tool'
 set :repo_url, 'git@github.com:the-codetrane/rsc-meeting-notes-app.git'
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 
