491b301b89ec83bea4a46a1ff7e36ce336455ed6
Michael Cain <michaelmcain@yahoo.com>
2017-03-24 22:05:34 -0400

    getting there!

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/do_new_rs.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id.pub) }
 set :puma_preload_app, true
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
