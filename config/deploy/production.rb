834af91cee8956bc256ef04cb405b37af429d783
Michael Cain <michaelmcain@yahoo.com>
2017-03-24 20:28:54 -0400

    added puma and nginx config for real I hope

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 set :branch, 'master'
 
-role [:web, :db, :app], '138.197.43.52', user: 'naworks4', primary: true
+role [:web, :db, :app], '138.197.43.52', user: 'root', primary: true
