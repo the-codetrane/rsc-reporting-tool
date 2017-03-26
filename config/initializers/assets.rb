03812cca305645810210db5aab3989bf882fc275
Michael Cain <michaelmcain@yahoo.com>
2017-03-26 12:41:24 -0400

    now will the glyphs compile?

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += [/scss/]
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+Rails.application.config.assets.precompile += %w(*.eot *.svg *.ttf *.woff *.woff2)
