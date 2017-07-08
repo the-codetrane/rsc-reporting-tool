443d8f6f54004a89f3f09ba4efda87ab84a72b69
Stephen Horne <stephenjhorne@gmail.com>
2017-07-07 21:03:17 -0400

    Cleared old Reports Controller to Prepare for Repurposing

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,66 +1,25 @@
 class ReportsController < ApplicationController
   def index
-    @reports = Report.includes(:committee, :user).order(updated_at: :desc).all
-    @users = User.all
-    @committees = SubCommittee.all
-    respond_to do |format|
-      format.html
-      format.json {@reports.to_json}
-    end
   end
 
   def show
-    @report = Report.find(params[:id])
   end
 
   def new
-    @report = Report.new
   end
 
   def create
-    @report = Report.new(report_params)
-
-    if @report.save
-      flash[:success] = 'Your Report Has Been Submitted'
-      redirect_to reports_path
-    else
-      flash[:alert] = 'Record did not save'
-      redirect_to new_report_path
-    end
   end
 
   def edit
-    @report = Report.find(params[:id])
   end
 
   def update
-    @report = Report.find(params[:id])
-    @report.update(report_params)
-
-    if @report
-      flash[:success] = 'Your Report Has Been Updated'
-      redirect_to reports_path
-    else
-      flash[:alert] = 'Something went wrong. Please try again'
-      redirect_to new_report_path
-    end
   end
 
   def destroy
-    Report.find(params[id]).destroy
-  end
-
-  def send_committee_report_email
-    report = SubCommitteeReport.find(params[:id])
-    ReportsMailer.report_email(report.committee).deliver_later
-    flash[:success]= 'Report successfully emailed'
-    redirect_to :back
   end
 
   private
-
-  def report_params
-    params.require(:report).permit!
-  end
 
 end
