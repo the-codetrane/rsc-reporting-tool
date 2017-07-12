6eb50fb34f1ad15728ceea60f126536b752e87a2
Stephen Horne <stephenjhorne@gmail.com>
2017-07-12 19:04:43 -0400

    Much improved seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,25 +18,25 @@
 # end
 
 User.destroy_all
-Group.all.each do |group|
+
+  30.times do
     User.create!(email: Faker::Internet.unique.email, password: Faker::Internet.password, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
-      group_id: group.id, sub_committee_id: 2, role_id: 1)
-end
+      group_id: Group.all.sample.id, sub_committee_id: 2, role_id: 1)
+  end
 
 AreaReport.destroy_all
-Group.all.each do |group|
-  3.times do
+
+  300.times do
     attendees = ''
     (rand * 10).to_i.times {attendees << Faker::Name.name + ' '}
 
     AreaReport.create!(title: Faker::Lorem.sentence, attendees: attendees, area_status: Faker::Lorem.sentence,
     area_needs: Faker::Lorem.paragraph, events: Faker::Lorem.paragraph,
     news: Faker::Lorem.paragraph, motions: Faker::Lorem.paragraph, notes: Faker::Lorem.paragraph,
-    area_id: Area.all.map {|area| area.id}.sample, created_at: Faker::Date.backward(365),
-    updated_at: Faker::Date.backward(365), created_by: User.all.where(group_id: group.id).sample.email,
+    area_id: Area.all.sample.id, created_at: Faker::Date.backward(365),
+    updated_at: Faker::Date.backward(365), created_by: User.all.sample.email,
     edited_by: 'N.A.', donation: Faker::Commerce.price * 100)
   end
-end
 
 puts "#{AreaReport.all.count} area reports successfully created."
 puts "#{User.all.count} users successfully created."
