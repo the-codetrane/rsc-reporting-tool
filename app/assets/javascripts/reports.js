a4f9f30f38a4c036d93e2ac0ade31f4cc1308afa
Stephen Horne <stephenjhorne@gmail.com>
2017-07-15 21:40:50 -0400

    Submit forms by selecting group or month, end-justifies-the-means jquery manipulation of tooltips via event listeners included in AJAX responses

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on('ready', function() {
+  $('select').on('change', function() {
+    $('select').submit();
+  })
+})
